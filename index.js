820d1e58709bf6ed5a6df8cb17dde2fe3036e2f7
Carlos Montes <carlos.montes88@gmail.com>
2023-08-17 18:56:45 -0500

    The connection to the database (MongoDB) is created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // NodeJS configuration file, server creation, connection to the database
+'use strict'
+
+var mongoose = require('mongoose'); // Import mongoose
+
+// Promise
+mongoose.Promise = global.Promise;
+
+// to connect to the database, the URL of the database is passed to it as a parameter
+// MongoDB default port: 27017
+mongoose.connect('mongodb://0.0.0.0:27017/portfolio')
+        .then(() => {
+            console.log("Database connection established successfully...");
+        })
+        .catch(err => console.log(err)); // Show the error
