d6cf94f348f9f0b197c19c977cbeaf357876006c
Carlos Montes <carlos.montes88@gmail.com>
2023-08-17 23:41:01 -0500

    server creation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 'use strict'
 
 var mongoose = require('mongoose'); // Import mongoose
+var app = require('./app'); // Import express
+var port = 3700;
 
 // Promise
 mongoose.Promise = global.Promise;
@@ -11,5 +13,10 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://0.0.0.0:27017/portfolio')
         .then(() => {
             console.log("Database connection established successfully...");
+
+            // Server creation
+            app.listen(port, () => {
+                console.log("Server running correctly in the url: localhost:3700");
+            });
         })
         .catch(err => console.log(err)); // Show the error
